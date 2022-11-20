9ec3fbf9b7838f691bb8681fbb35d8c00b86b439
dahye <dhk8233@gmail.com>
2022-11-20 20:49:54 +0900

    first

---
+++ Book_search_App/app.js
@@ -0,0 +1,4 @@
+// const apiKey = 'ttbrlaekgp82331907003'
+// const URL = `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${apiKey}&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101
+// `
+
