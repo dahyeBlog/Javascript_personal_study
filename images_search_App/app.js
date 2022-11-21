9ac3ef0452cea283600837857b6c3f1a07b6a756
dahye <dhk8233@gmail.com>
2022-11-21 16:57:01 +0900

    images_search_app_ajax_jquery

--- images_search_App/app.js
+++ images_search_App/app.js
@@ -1,24 +1,37 @@
 const apiKey = "KakaoAK 7c98d915856717af7c7faa2ddcfef9d2";
-const url = `https://dapi.kakao.com/v2/search/image?`;
+const url = `https://dapi.kakao.com/v2/search/image`;
+
+const imagesDOM = document.querySelector('.images-center');
+
+
 
 function imageSearch() {
   let search = document.querySelector(".search-input").value;
-
-  console.log(search);
-
   $.ajax({
     type: "GET",
     url: url,
 
     data: {
-      query: `${search}`,
+      query: search,
     },
     headers: {
       Authorization: "KakaoAK 7c98d915856717af7c7faa2ddcfef9d2",
     },
+   
+
   }).done((msg) => {
-    console.log(msg);
-  });
+    const imageList = msg.documents.map((image) => {
+      // console.log(image);
+      const {image_url,thumbnail_url} = image
+      return `<a href="${image_url}">
+        <img src="${thumbnail_url}" alt="" /></a>`
+    }).join('')
+    
+    imagesDOM.innerHTML = imageList
+  })
 }
 
-document.querySelector(".btn").addEventListener("click", imageSearch);
+document.querySelector(".btn").addEventListener("click", (event)=>{
+  event.preventDefault()
+  imageSearch()
+});
